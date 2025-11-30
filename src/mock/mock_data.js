676f43c2be27021d351818ba4a980b44be97f737
SOYS1 <109346265+SOYS1@users.noreply.github.com>
2025-11-30 20:13:19 +0800

    chore: 更新导航数据 - 2025/11/30 20:13:18

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -1,4 +1,42 @@
 export const mockData = {
-  "categories": [],
+  "categories": [
+    {
+      "id": "category-1764504497885",
+      "icon": "🎬",
+      "name": "视频娱乐",
+      "order": 0,
+      "sites": [
+        {
+          "id": "site-1764504594603",
+          "name": "哔哩哔哩弹幕视频网Σ(っ°Д°;)っ",
+          "url": "https://www.bilibili.com/",
+          "description": "",
+          "icon": "https://www.faviconextractor.com/favicon/www.bilibili.com"
+        },
+        {
+          "id": "site-1764504786128",
+          "name": "免费看日本漫画",
+          "url": "https://www.yymanhua.com/",
+          "description": "",
+          "icon": "https://www.faviconextractor.com/favicon/www.yymanhua.com"
+        }
+      ]
+    },
+    {
+      "id": "category-1764504522783",
+      "icon": "📚",
+      "name": "小说漫画",
+      "order": 1,
+      "sites": [
+        {
+          "id": "site-1764504758276",
+          "name": "程序员书吧",
+          "url": "https://www.cxysb.com/",
+          "description": "",
+          "icon": "https://www.faviconextractor.com/favicon/www.cxysb.com"
+        }
+      ]
+    }
+  ],
   "title": "夕阳红🌇导航"
 }
